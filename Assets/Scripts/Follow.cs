fc421c79320031302cabd36a3d4713b67d2c58d5
Pudge2016 <chydik322@gmail.com>
2024-05-09 10:36:40 +0300

    1

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject goal;
+
+    [Range(0f, 5f)]
+    public float speed = 0.05f;
+ 
+    private Vector3 direction;
+    private Vector3 velocity;
+
+    private void LateUpdate()
+    {
+        direction = goal.transform.position - transform.position;
+        velocity = direction.normalized * speed * Time.deltaTime;
+
+        if (direction.magnitude > 2)
+        {
+            transform.LookAt(goal.transform.position);
+            transform.position = transform.position + velocity;
+        }
+    }
+}
